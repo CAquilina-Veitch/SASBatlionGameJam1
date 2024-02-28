ad840ffaabbb767306166e3ad1f6d445b1946be1
Cody <1027166@student.sae.edu.au>
2024-02-28 12:17:36 +0800

    mario

---
+++ Assets/MaroController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaroController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    float xVel;
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private void Update()
+    {
+        xVel = Mathf.Lerp(rb.velocity.x, Input.GetAxisRaw("Horizontal"),0.01f);
+
+        float rJump = ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && rb.velocity.y == 0)?jumpForce:rb.velocity.y;
+       
+        rb.velocity = new Vector3(xVel*moveSpeed, rJump);
+
+    }
+
+
+
+}
